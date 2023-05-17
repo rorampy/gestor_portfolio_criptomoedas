687a21bd95dd630750b19841e3802a1d4f815e1e
rorampy <rorampy@gmail.com>
2023-05-17 15:34:43 -0300

    alteração para comunicação com o banco de dados mysql

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
 
 DEBUG = True
+
+USERNAME = 'root'
+PASSWORD = ''
+SERVER = 'localhost'
+DB = 'gestor_portfolio_cripto'
+
+SQLALCHEMY_DATABASE_URI = f'mysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}'
+SQLALCHEMY_TRACK_NOTIFICATIONS = True
